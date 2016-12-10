c4ccb6b4a566570c119bb9d09b5f99c341c0094c
monoid911 <onixie@gmail.com>
2016-12-10 20:47:37 +0900

    Try output camera to local file.
    1. Package FFmpeg with apk as a .so files.
    2. Let FFmpeg receive image2pipe inputs and output to file.
    4. Register PreviewCallback to get frame and convert to jpeg.
    5. Fix some init/destroy error.

--- app/src/main/java/com/monoid911/cam2ff/MainActivity.java
+++ app/src/main/java/com/monoid911/cam2ff/MainActivity.java
@@ -1,46 +1,96 @@
 package com.monoid911.cam2ff;
 
 import android.app.Activity;
+import android.graphics.ImageFormat;
+import android.graphics.Rect;
+import android.graphics.YuvImage;
 import android.hardware.Camera;
 import android.os.Bundle;
 import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
-public class MainActivity extends Activity {
-    Camera mCamera;
-    SurfaceView mSurfaceView;
-    private SurfaceHolder.Callback callback = new SurfaceHolder.Callback() {
-        @Override
-        public void surfaceCreated(SurfaceHolder surfaceHolder) {
+import java.io.File;
+import java.io.IOException;
 
-            //CameraOpen
-            mCamera = Camera.open(1);
-            setCameraDisplayOrientation(1, mCamera);
-            //出力をSurfaceViewに設定
-            try {
-                mCamera.setPreviewDisplay(surfaceHolder);
-            } catch (Exception e) {
-                e.printStackTrace();
+public class MainActivity extends Activity {
+    private Camera mCamera;
+    private SurfaceView mSurfaceView;
+    private Process mffmpegProc;
+    private SurfaceHolder.Callback mSHCallback;
+
+    public MainActivity() {
+    }
+
+    private Camera.PreviewCallback newPreviewCallback() {
+        return new Camera.PreviewCallback() {
+            @Override
+            public void onPreviewFrame(byte[] bytes, Camera camera) {
+                Camera.Parameters param = camera.getParameters();
+                Camera.Size size = param.getPreviewSize();
+                int format = param.getPreviewFormat();
+                int bufferSize = size.width * size.height * ImageFormat.getBitsPerPixel(format);
+
+                // JPEGに変換してffmpegに転送する
+                new YuvImage(bytes, format, size.width, size.height, null)
+                        .compressToJpeg(new Rect(0, 0, size.width, size.height), 50, mffmpegProc.getOutputStream());
+
+                camera.addCallbackBuffer(new byte[bufferSize]);
+            }
+        };
+    }
+
+    private SurfaceHolder.Callback newSurfaceHolderCallback() {
+        return new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder surfaceHolder) {
+                // Start ffmpeg
+                String cwd = "/data/data/" + getPackageName();
+                String ffmpeg = cwd + "/lib/libffmpeg.so"; // lollipopまで*.soはここにインストールするそうだ。
+                String output = cwd + "/output.flv";
+
+                try {
+                    mffmpegProc = new ProcessBuilder()
+                            .command(ffmpeg, "-y", "-f", "image2pipe", "-i", "-", "-f", "flv", output)
+                            .directory(new File(cwd))
+                            .redirectErrorStream(true)
+                            .start();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                // Open camera
+                mCamera = Camera.open(1);
+                setCameraDisplayOrientation(1, mCamera);
+                try {
+                    mCamera.setPreviewDisplay(surfaceHolder);
+                    mCamera.setPreviewCallback(newPreviewCallback());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
 
-        }
+            @Override
+            public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i2, int i3) {
+                //プレビュースタート（Changedは最初にも1度は呼ばれる）
+                setCameraDisplayOrientation(1, mCamera);
+                mCamera.startPreview();
+            }
 
-        @Override
-        public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i2, int i3) {
-            setCameraDisplayOrientation(1, mCamera);
-            //プレビュースタート（Changedは最初にも1度は呼ばれる）
-            mCamera.startPreview();
-        }
-
-        @Override
-        public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
-
-            //片付け
-            mCamera.release();
-            mCamera = null;
-        }
-    };
+            @Override
+            public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+                //片付け
+                try {
+                    mCamera.setPreviewDisplay(null);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                mCamera.setPreviewCallback(null);
+                mCamera.release();
+                mffmpegProc.destroy();
+            }
+        };
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,10 +104,29 @@ public class MainActivity extends Activity {
 
         mSurfaceView = (SurfaceView) findViewById(com.monoid911.cam2ff.R.id.surfaceView);
         SurfaceHolder holder = mSurfaceView.getHolder();
-        holder.addCallback(callback);
+        mSHCallback = newSurfaceHolderCallback();
+        holder.addCallback(mSHCallback);
     }
 
-    public void setCameraDisplayOrientation(int cameraId, android.hardware.Camera camera) {
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        mCamera.setPreviewCallback(null);
+        try {
+            mCamera.setPreviewDisplay(null);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        mCamera.release();
+        mffmpegProc.destroy();
+
+        mSurfaceView = (SurfaceView) findViewById(com.monoid911.cam2ff.R.id.surfaceView);
+        SurfaceHolder holder = mSurfaceView.getHolder();
+        holder.removeCallback(mSHCallback);
+    }
+
+    private void setCameraDisplayOrientation(int cameraId, android.hardware.Camera camera) {
         android.hardware.Camera.CameraInfo info =
                 new android.hardware.Camera.CameraInfo();
         android.hardware.Camera.getCameraInfo(cameraId, info);
