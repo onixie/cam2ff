4284111afc0bfca285a7e107707a1e14e155c7b7
monoid911 <onixie@gmail.com>
2017-01-08 22:11:40 +0900

    Add Settings activity and refactoring
    1. Add Settings fragment and switching view code
    2. Extract camera and ffmpeg code out to private method.

--- app/src/main/java/com/monoid911/cam2ff/MainActivity.java
+++ app/src/main/java/com/monoid911/cam2ff/MainActivity.java
@@ -1,15 +1,22 @@
 package com.monoid911.cam2ff;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.graphics.ImageFormat;
 import android.graphics.Rect;
 import android.graphics.YuvImage;
 import android.hardware.Camera;
 import android.os.Bundle;
 import android.os.Handler;
+import android.preference.PreferenceManager;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 import java.io.BufferedReader;
@@ -18,19 +25,29 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ExecutorService;
 
 import static java.util.concurrent.Executors.newFixedThreadPool;
 
 public class MainActivity extends Activity {
+    private final Handler handler = new Handler();
+    private final int delay = 1000; //milliseconds
     private Camera mCamera;
     private SurfaceView mSurfaceView;
     private Process mffmpegProc;
     private boolean mffmpegProcExit;
     private SurfaceHolder.Callback mSHCallback;
-    private TextView mFFmpegLog;
-    private String log;
+    private TextView mFFmpegOutput;
+    private String ffmpegOutput;
     private ExecutorService mPool = newFixedThreadPool(1);
+    private SharedPreferences mPreferences;
+    private Boolean showOutput;
+    private String serverAddress;
+    private String serverPort;
+    private Size resolution = new Size();
+    private Integer jpegQuality;
+
     public MainActivity() {
     }
 
@@ -53,7 +70,7 @@ public class MainActivity extends Activity {
 
                 // JPEGに変換してffmpegに転送する
                 new YuvImage(bytes, format, rect.width(), rect.height(), null)
-                        .compressToJpeg(rect, 100, output);
+                        .compressToJpeg(rect, jpegQuality, output);
                 if (mffmpegProcExit) {
                     android.os.Process.killProcess(android.os.Process.myPid());
                 }
@@ -65,60 +82,8 @@ public class MainActivity extends Activity {
         return new SurfaceHolder.Callback() {
             @Override
             public void surfaceCreated(SurfaceHolder surfaceHolder) {
-                // Start ffmpeg
-                String cwd = "/data/data/" + getPackageName();
-                String ffmpeg = cwd + "/lib/libffmpeg.so"; // lollipopまで*.soはここにインストールするそうだ。
-                String output = cwd + "/output.flv";
-
-                try {
-                    mffmpegProcExit = false;
-                    mffmpegProc = new ProcessBuilder()
-                            .command(ffmpeg, "-f", "image2pipe", "-i", "-", "-f", "ffm", "http://192.168.100.105:8090/feed")
-                            .directory(new File(cwd))
-                            .redirectErrorStream(true)
-                            .start();
-
-                    new Thread(new Runnable() {
-                        @Override
-                        public void run() {
-                            BufferedReader br = new BufferedReader(new InputStreamReader(mffmpegProc.getInputStream()));
-                            try {
-                                while (true) {
-                                    if (br.ready()) {
-                                        log = br.readLine();
-                                        System.out.println(log);
-                                    }
-                                }
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                            }
-                        }
-                    }).start();
-                    new Thread(new Runnable() {
-                        @Override
-                        public void run() {
-                            try {
-                                mffmpegProc.waitFor();
-                            } catch (InterruptedException e) {
-                                e.printStackTrace();
-                            }
-                            mffmpegProcExit = true;
-                        }
-                    }).start();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-
-                // Open camera
-                mCamera = Camera.open(1);
-                setCameraResolution(mCamera);
-                setCameraDisplayOrientation(1, mCamera);
-                try {
-                    mCamera.setPreviewDisplay(surfaceHolder);
-                    mCamera.setPreviewCallback(newPreviewCallback());
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
+                startFFmpeg();
+                startCamera(surfaceHolder);
             }
 
             @Override
@@ -129,21 +94,9 @@ public class MainActivity extends Activity {
 
             @Override
             public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
-                if (mCamera != null) {
-                    mCamera.setPreviewCallback(null);
-                    try {
-                        mCamera.setPreviewDisplay(null);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                    mCamera.release();
-                    mCamera = null;
-                }
-
-                if (mffmpegProc != null) {
-                    mffmpegProc.destroy();
-                    mffmpegProc = null;
-                }
+                stopFFmpeg();
+                stopCamera();
+                removeSurfaceHolderCallback(surfaceHolder);
             }
         };
     }
@@ -151,36 +104,139 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(com.monoid911.cam2ff.R.layout.activity_main);
+        setContentView(com.monoid911.cam2ff.R.layout.main);
+        PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
 
-        final Handler updateLog = new Handler();
-        final int delay = 1000; //milliseconds
-        updateLog.postDelayed(new Runnable() {
-            public void run() {
-
-                mFFmpegLog = (TextView) findViewById(com.monoid911.cam2ff.R.id.ffmpegOutput);
-                if (mFFmpegLog != null) {
-                    mFFmpegLog.setText(log);
-                }
-                updateLog.postDelayed(this, delay);
-            }
-        }, delay);
+        updatePreferences();
+        hookFFmpegOutput();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        mSurfaceView = (SurfaceView) findViewById(com.monoid911.cam2ff.R.id.surfaceView);
-        mSurfaceView.setKeepScreenOn(true);
-        SurfaceHolder holder = mSurfaceView.getHolder();
-        mSHCallback = newSurfaceHolderCallback();
-        holder.addCallback(mSHCallback);
+
+        showCaptureView();
     }
 
     @Override
     protected void onPause() {
         super.onPause();
 
+        stopFFmpeg();
+        stopCamera();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.settings:
+                showSettingsView();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void onBack(View v) {
+        showCaptureView();
+    }
+
+    /*  View */
+    private void showSettingsView() {
+        final ViewGroup viewGroup = (ViewGroup) findViewById(R.id.group);
+        viewGroup.removeAllViews();
+        viewGroup.addView(View.inflate(this, R.layout.settings, null));
+
+        SettingsFragment settings = new SettingsFragment();
+        getFragmentManager().beginTransaction().replace(R.id.settings, settings).commit();
+    }
+
+    private void showCaptureView() {
+        updatePreferences();
+        if (serverAddress == null || serverAddress.split("\\.").length != 4) {
+            showSettingsView();
+            return;
+        }
+
+        final ViewGroup viewGroup = (ViewGroup) findViewById(R.id.group);
+        viewGroup.removeAllViews();
+        viewGroup.addView(View.inflate(this, R.layout.capture, null));
+
+        addSurfaceHolderCallback();
+    }
+
+    private void updatePreferences() {
+        Map<String, ?> pref = PreferenceManager.getDefaultSharedPreferences(this).getAll();
+
+        serverAddress = (String) pref.get("serverAddress");
+        serverPort = (String) pref.get("serverPort");
+        showOutput = (Boolean) pref.get("showOutput");
+
+        String[] wh = ((String) pref.get("resolution")).split("x");
+        Integer width = null, height = null;
+        if (wh.length == 2) {
+            width = Integer.getInteger(wh[0]);
+            height = Integer.getInteger(wh[1]);
+        }
+
+        if (width != null && height != null) {
+            resolution.width = width;
+            resolution.height = height;
+        } else {
+            resolution.width = 320;
+            resolution.height = 240;
+        }
+
+        jpegQuality = Integer.getInteger((String) pref.get("jpegQuality"));
+        if (jpegQuality == null) {
+            jpegQuality = 100;
+        }
+    }
+
+    /*  Preview Surface */
+    private void addSurfaceHolderCallback() {
+        mSurfaceView = (SurfaceView) findViewById(com.monoid911.cam2ff.R.id.surfaceView);
+        if (mSurfaceView != null) {
+            mSurfaceView.setKeepScreenOn(true);
+            SurfaceHolder holder = mSurfaceView.getHolder();
+            if (mSHCallback == null) {
+                mSHCallback = newSurfaceHolderCallback();
+                holder.addCallback(mSHCallback);
+            }
+        }
+    }
+
+    private void removeSurfaceHolderCallback(SurfaceHolder holder) {
+        if (mSHCallback != null) {
+            holder.removeCallback(mSHCallback);
+            mSHCallback = null;
+        }
+    }
+
+    /*  Camera */
+    private void startCamera(SurfaceHolder surfaceHolder) {
+        if (mCamera == null && surfaceHolder != null) {
+            // Open camera
+            mCamera = Camera.open(1);
+            setCameraResolution(mCamera);
+            setCameraDisplayOrientation(1, mCamera);
+            try {
+                mCamera.setPreviewDisplay(surfaceHolder);
+                mCamera.setPreviewCallback(newPreviewCallback());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void stopCamera() {
         if (mCamera != null) {
             mCamera.setPreviewCallback(null);
             try {
@@ -191,23 +247,15 @@ public class MainActivity extends Activity {
             mCamera.release();
             mCamera = null;
         }
-
-        if (mffmpegProc != null) {
-            mffmpegProc.destroy();
-            mffmpegProc = null;
-        }
-
-        mSurfaceView = (SurfaceView) findViewById(com.monoid911.cam2ff.R.id.surfaceView);
-        SurfaceHolder holder = mSurfaceView.getHolder();
-        holder.removeCallback(mSHCallback);
     }
 
     private void setCameraResolution(android.hardware.Camera camera) {
         Camera.Parameters params = mCamera.getParameters();
         List<Camera.Size> supported = params.getSupportedPreviewSizes();
-        params.setPreviewSize(320, 240);
+        params.setPreviewSize(resolution.width, resolution.height);
         mCamera.setParameters(params);
     }
+
     private void setCameraDisplayOrientation(int cameraId, android.hardware.Camera camera) {
         android.hardware.Camera.CameraInfo info =
                 new android.hardware.Camera.CameraInfo();
@@ -238,5 +286,83 @@ public class MainActivity extends Activity {
             result = (info.orientation - degrees + 360) % 360;
         }
         camera.setDisplayOrientation(result);
+    }
+
+    /*  FFmpeg */
+    private void startFFmpeg() {
+        if (mffmpegProc == null) {
+            // Start ffmpeg
+            String cwd = "/data/data/" + getPackageName();
+            String ffmpeg = cwd + "/lib/libffmpeg.so"; // lollipopまで*.soはここにインストールするそうだ。
+            String output = cwd + "/output.flv";
+
+            try {
+                mffmpegProcExit = false;
+                mffmpegProc = new ProcessBuilder()
+                        .command(ffmpeg, "-f", "image2pipe", "-i", "-", "-f", "ffm", String.format("http://%s:%s/feed", serverAddress, serverPort))
+                        .directory(new File(cwd))
+                        .redirectErrorStream(true)
+                        .start();
+
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        BufferedReader br = new BufferedReader(new InputStreamReader(mffmpegProc.getInputStream()));
+                        try {
+                            while (showOutput) {
+                                if (br.ready()) {
+                                    ffmpegOutput = br.readLine();
+                                    System.out.println(ffmpegOutput);
+                                }
+                            }
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }).start();
+
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            mffmpegProc.waitFor();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        mffmpegProcExit = true;
+                    }
+                }).start();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void stopFFmpeg() {
+        if (mffmpegProc != null) {
+            mffmpegProc.destroy();
+            mffmpegProc = null;
+        }
+    }
+
+    private void hookFFmpegOutput() {
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                mFFmpegOutput = (TextView) findViewById(com.monoid911.cam2ff.R.id.ffmpegOutput);
+                if (mFFmpegOutput != null) {
+                    if (showOutput) {
+                        mFFmpegOutput.setText(ffmpegOutput);
+                    } else {
+                        mFFmpegOutput.setText("");
+                    }
+                }
+                handler.postDelayed(this, delay);
+            }
+        }, delay);
+    }
+
+    private class Size {
+        int width;
+        int height;
     }
 }
